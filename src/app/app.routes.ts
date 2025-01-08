e40dc55fcef2176accba41c8eff9f5a80d266e58
Bohdan Petrenko <bohdan.petrenko.web@gmail.com>
2025-01-08 20:59:53 +0100

    Update routing: add LoanDashboardComponent as the default route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { LoanDashboardComponent } from './loan-dashboard/loan-dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{path: '', component: LoanDashboardComponent}
+];
