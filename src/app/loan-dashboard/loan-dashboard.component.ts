4236ff83cbfa928ee57af45a4a212cb23458e5f7
Bohdan Petrenko <bohdan.petrenko.web@gmail.com>
2025-01-15 20:53:51 +0100

    Enhance LoanDashboardComponent: Added Angular Material modules, form controls, and data bindings

--- src/app/loan-dashboard/loan-dashboard.component.ts
+++ src/app/loan-dashboard/loan-dashboard.component.ts
@@ -1,19 +1,39 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { OnInit } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatTableModule } from '@angular/material/table';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-loan-dashboard',
-	standalone: true,
-  imports: [CommonModule],
+  standalone: true,
   templateUrl: './loan-dashboard.component.html',
-  styleUrl: './loan-dashboard.component.scss'
+  styleUrls: ['./loan-dashboard.component.scss'],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatTableModule,
+    MatSelectModule,
+    MatInputModule,
+    MatFormFieldModule,
+		MatCardModule,
+  ],
 })
-export class LoanDashboardComponent implements OnInit {
-	constructor() { }
+export class LoanDashboardComponent {
 
-	ngOnInit() {
-	}
+  rateType = 'równa'; 
+  postOverpayment = 'lower installment';
+
+
+  totalPayments = 0; 
+  totalOverpayment = 0; 
+  totalCost = 0; 
+  repaymentTime = 300; 
+  profit = 'brak'; 
+
+	
 
 }
