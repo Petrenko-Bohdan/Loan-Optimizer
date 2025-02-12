b22f61e64b8e6830e5f6699ee24b0fac6b252219
Bohdan Petrenko <bohdan.petrenko.web@gmail.com>
2025-02-12 20:50:38 +0100

    Create loan.effects

---
+++ src/app/store/loan.effects.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Actions, ofType, createEffect } from '@ngrx/effects';
+import { Store } from '@ngrx/store';
+import { of } from 'rxjs';
+import { map, mergeMap, catchError } from 'rxjs/operators';
+
+
+import { Loan } from '../models/loan.model';
+
+@Injectable()
+export class LoanEffects{
+	constructor(
+		private actions$: Actions,
+		
+		private store: Store
+	){}	
+}
