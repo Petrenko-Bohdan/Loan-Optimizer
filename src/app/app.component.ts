71746723e880c1ab67149de608f4d9bdacaf14ac
Bohdan Petrenko <bohdan.petrenko.web@gmail.com>
2025-02-12 20:43:24 +0100

    add LoanDetailsComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoanDetailsComponent } from './loan-details/loan-details.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoanDetailsComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'Loan-Optimizer';
