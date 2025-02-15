3f0597301c7a9dde4364b5356e48a09824266004
Bohdan Petrenko <bohdan.petrenko.web@gmail.com>
2025-02-15 17:56:45 +0100

    Add LoanFormComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoanDetailsComponent } from './loan-details/loan-details.component';
+import { LoanFormComponent } from './loan-form/loan-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoanDetailsComponent],
+  imports: [RouterOutlet, LoanDetailsComponent, LoanFormComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
