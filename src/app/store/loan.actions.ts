2ae790bc806ceda15a002df9eb362d75df19676a
Bohdan Petrenko <bohdan.petrenko.web@gmail.com>
2025-04-07 20:56:51 +0200

    add calculateLoanValid /  calculateLoanValidSuccess

--- src/app/store/loan.actions.ts
+++ src/app/store/loan.actions.ts
@@ -1,6 +1,7 @@
 import{createAction} from '@ngrx/store';
 import { props } from '@ngrx/store';
 import { Loan } from '../models/loan.model';
+import { loanForm } from '../models/loan.model';
 
 export const calculateLoan = createAction(
 	'[Loan] Calculate Loan',
@@ -10,4 +11,13 @@ export const calculateLoan = createAction(
 export const calculateLoanSuccess= createAction(
 	'[Loan] Calculate Loan Success',
 	props<{paymentSchedule: any[]}>()
+)
+
+export const calculateLoanValid= createAction(
+	'[Loan] Calculate Loan Valid',
+	props<{loan:loanForm}>())
+
+export const calculateLoanValidSuccess= createAction(
+	'[Loan] Calculate Loan Valid Success',
+	props<{paymentScheduleValid: any[]}>()
 )
