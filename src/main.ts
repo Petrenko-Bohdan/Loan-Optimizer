edf47691be17e487c0dd3e8ae93cc1ca8574b7c6
Bohdan Petrenko <bohdan.petrenko.web@gmail.com>
2025-04-17 18:45:45 +0200

    feat(app): configure bootstrap with NgRx store and animations
    - Replaced old bootstrap logic with enhanced provider configuration
    - Integrated `provideStore` to set up NgRx store with loan reducer
    - Added `provideAnimations` for Angular Material animations support

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideAnimations } from '@angular/platform-browser/animations'; // <-- импорт
 import { AppComponent } from './app/app.component';
+import { provideStore } from '@ngrx/store';
+import { loanReducer } from './app/store/calculate-loan/calculate-loan.reducer';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideStore({ loan: loanReducer }),
+    provideAnimations(), 
+  ],
+});
