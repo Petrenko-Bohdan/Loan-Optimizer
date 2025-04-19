6cba782ebfec36c64f7c715a0bbac4de5804d6c2
Bohdan Petrenko <bohdan.petrenko.web@gmail.com>
2025-04-19 14:00:55 +0200

    feat(app): add Angular Router configuration to application bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideAnimations } from '@angular/platform-browser/animations'; // <-- импорт
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { AppComponent } from './app/app.component';
 import { provideStore } from '@ngrx/store';
 import { loanReducer } from './app/store/calculate-loan/calculate-loan.reducer';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideStore({ loan: loanReducer }),
     provideAnimations(), 
+		provideRouter(routes)
   ],
 });
