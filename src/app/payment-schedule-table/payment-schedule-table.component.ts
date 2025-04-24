844a4b6067e301f7ebe8682593dca4a7da6142bd
Bohdan Petrenko <bohdan.petrenko.web@gmail.com>
2025-04-24 19:08:06 +0200

    Refactor PaymentScheduleTableComponent to use NgRx store and display extended payment data"

--- src/app/payment-schedule-table/payment-schedule-table.component.ts
+++ src/app/payment-schedule-table/payment-schedule-table.component.ts
@@ -1,15 +1,27 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
-import {MatTableModule} from '@angular/material/table';
+import { MatTableModule } from '@angular/material/table';
+import { Observable } from 'rxjs';
+import { AppState } from '../store/calculate-loan/app.state';
+import { selectLoanResult } from '../store/calculate-loan/calculate-loan.selectors';
+import { Store } from '@ngrx/store';
+import { CommonModule } from '@angular/common';
+import { MatCommonModule } from '@angular/material/core';
 
 
 @Component({
   selector: 'app-payment-schedule-table',
   standalone: true,
-  imports: [MatCardModule, MatTableModule],
+  imports: [MatCardModule, MatTableModule, CommonModule, MatCommonModule],
   templateUrl: './payment-schedule-table.component.html',
   styleUrl: './payment-schedule-table.component.scss'
 })
 export class PaymentScheduleTableComponent {
-	displayedColumns: string[] = ['month', 'capital', 'interest', 'total'];
+	displayedColumns: string[] = ['month','capital', 'interest', 'installment', 'total'];
+
+	paymentSchedule$: Observable<any[]>;
+
+	constructor(private store: Store<AppState>){
+		this.paymentSchedule$=this.store.select(selectLoanResult)
+	}
 }
