00a3e630820dedca891a41b79290a02dbda2e44a
Bohdan Petrenko <bohdan.petrenko.web@gmail.com>
2025-04-25 20:53:29 +0200

    Add loanResult  interface

--- src/app/models/loan.model.ts
+++ src/app/models/loan.model.ts
@@ -1,18 +1,27 @@
-export interface instalmentTypes{
-	value: string;
-	viewValue: string;
+export interface instalmentTypes {
+  value: string;
+  viewValue: string;
 }
 
 export interface afterOverpayment {
-	value: string;
-	viewValue: string;
+  value: string;
+  viewValue: string;
 }
 
 export interface loanForm {
-	instalmentType: string;
-	loanAmount: number;
-	loanTerm: number;
-	interestRate: number;
-	afterOverpayment: string;
+  instalmentType: string;
+  loanAmount: number;
+  loanTerm: number;
+  interestRate: number;
+  afterOverpayment: string;
+}
+
+export interface loanResult {
+  month: number;
+  capital: number;
+  interest: number;
+  installment: number;
+  total: number;
+  overpayment: number;
 }
 
