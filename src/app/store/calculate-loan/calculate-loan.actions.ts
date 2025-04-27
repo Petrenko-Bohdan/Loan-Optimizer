88d22bb8a6306f0c42dffaa2c6def6134c833a84
Bohdan Petrenko <bohdan.petrenko.web@gmail.com>
2025-04-27 11:08:55 +0200

    Add updateOverpayment action to handle overpayment input changes"

--- src/app/store/calculate-loan/calculate-loan.actions.ts
+++ src/app/store/calculate-loan/calculate-loan.actions.ts
@@ -1,12 +1,18 @@
-import { createAction, props } from "@ngrx/store";
-import { loanForm } from "../../models/loan.model";
+import { createAction, props } from '@ngrx/store';
+import { loanForm, loanResult } from '../../models/loan.model';
 
 export const calculateLoan = createAction(
-	"[Loan] Calculate Loan",
-	props<{ loanData: loanForm }>()
-)
+  '[Loan] Calculate Loan',
+  props<{ loanData: loanForm }>()
+);
 
 export const calculateLoanSuccess = createAction(
-	"[Loan] Calculate Loan Success",
-	props<{ result: any}>()
-)
+  '[Loan] Calculate Loan Success',
+  props<{ result: any }>()
+);
+
+export const updateOverpayment = createAction(
+  '[Loan] Update Overpayment',
+  props<{ index: number; overpayment: number }>()
+);
+
