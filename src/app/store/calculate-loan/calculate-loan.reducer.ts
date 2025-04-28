88bbedca17a209f344f3f06b94f69ed46dee33fa
Bohdan Petrenko <bohdan.petrenko.web@gmail.com>
2025-04-28 20:47:30 +0200

    "Enhance loan reducer logic: - Implement overpayment updates and recalculation of payment schedule - Add updateOverpayment action and handle it in reducer - Improve financial precision (2-decimal formatting) - Allow early loan repayment by dynamically adjusting remaining balance - Maintain correct month indexing and truncate schedule when balance reaches 0 - Refactor calculatePaymentSchedule and add recalculatedPaymentSchedule function "

--- src/app/store/calculate-loan/calculate-loan.reducer.ts
+++ src/app/store/calculate-loan/calculate-loan.reducer.ts
@@ -1,6 +1,10 @@
 import { createReducer, on } from '@ngrx/store';
-import { calculateLoan, calculateLoanSuccess } from './calculate-loan.actions';
+import {
+  calculateLoan,
+  updateOverpayment,
+} from './calculate-loan.actions';
 import { loanForm } from '../../models/loan.model';
+
 
 export interface LoanState {
   result: loanForm | null;
@@ -15,40 +19,88 @@ export const initialState: LoanState = {
 export const loanReducer = createReducer(
   initialState,
   on(calculateLoan, (state, { loanData }) => {
-  
     return {
       ...state,
       result: loanData,
       paymentSchedule: calculatePaymentSchedule(loanData),
     };
   }),
-  
+
+  on(updateOverpayment, (state, { index, overpayment }) => {
+    const updatedSchedule = state.paymentSchedule.map((payment, i) => {
+      if (i === index) {
+        return { ...payment, overpayment: overpayment };
+      }
+      return payment;
+    });
+		const recalculatedSchedule = recalculatedPaymentSchedule(updatedSchedule, state.result!);
+
+		return {...state, paymentSchedule: recalculatedSchedule };
+  })
 );
 
 function calculatePaymentSchedule(loanData: loanForm): any[] {
-  const { instalmentType, loanAmount, loanTerm, interestRate, afterOverpayment } = loanData; 
-	const monthlyInterestRate = interestRate / 100 / 12;
-	const monthlyPayment = (loanAmount * monthlyInterestRate) / (1 - Math.pow(1 + monthlyInterestRate, -loanTerm));
+  const {
+    instalmentType,
+    loanAmount,
+    loanTerm,
+    interestRate,
+    afterOverpayment,
+  } = loanData;
+  const monthlyInterestRate = interestRate / 100 / 12;
+  const monthlyPayment =
+    (loanAmount * monthlyInterestRate) /
+    (1 - Math.pow(1 + monthlyInterestRate, -loanTerm));
 
-	const paymentSchedule = [];
-	let remainingBalance = loanAmount;
+  const paymentSchedule = [];
+  let remainingBalance = loanAmount;
 
-	for (let i=1; i<=loanTerm; i++) {
+  for (let i = 1; i <= loanTerm; i++) {
+    const interest = remainingBalance * monthlyInterestRate;
+    const principal = monthlyPayment - interest;
+
+    paymentSchedule.push({
+      month: i,
+      capital: remainingBalance.toFixed(2),
+      interest: interest.toFixed(2),
+      installment: principal.toFixed(2),
+      total: monthlyPayment.toFixed(2),
+    });
+
+    remainingBalance -= principal;
+  }
+
+  return paymentSchedule;
+}
+
+function recalculatedPaymentSchedule(paymentSchedule: any[], loanData: loanForm): any[]{
+	const monthlyInterestRate = loanData.interestRate / 100 / 12;
+	let remainingBalance = loanData.loanAmount;
+
+	for (let i=0; i<paymentSchedule.length; i++){
+		const overpayment = Number(paymentSchedule[i].overpayment) || 0;
+
 		const interest = remainingBalance * monthlyInterestRate;
-		const principal = monthlyPayment - interest;
-		
-		paymentSchedule.push({
-			month: i,
+		const principal = ((loanData.loanAmount*monthlyInterestRate)/(1-Math.pow(1+monthlyInterestRate, -loanData.loanTerm))) + overpayment;
+		const total = principal + interest;
+		remainingBalance -= (principal + overpayment);
+
+		if (remainingBalance < 0) {remainingBalance = 0;}
+
+		paymentSchedule[i]={...paymentSchedule[i],
+			month: i+1,
 			capital: remainingBalance.toFixed(2),
 			interest: interest.toFixed(2),
 			installment: principal.toFixed(2),
-			total: monthlyPayment.toFixed(2),
-		});
+			total: total.toFixed(2),
+			overpayment: overpayment ? overpayment:undefined,
+		};
 
-		remainingBalance -= principal;	
+		if(remainingBalance === 0){
+			paymentSchedule.length = i+1;
+			break;
+		}
 	}
 
-	return paymentSchedule;
-
+	return paymentSchedule
 }
-
