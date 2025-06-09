c31acaf97cd2ed81c0e049c151a9b15c178d669c
Bohdan Petrenko <bohdan.petrenko.web@gmail.com>
2025-06-09 18:21:18 +0200

    configure Firebase and enable analytics
    - Added firebaseConfig with project credentials
    - Initialized Firebase app using provideFirebaseApp()
    - Enabled Google Analytics via provideAnalytics()
    - Updated bootstrap logic to include Firebase services

---
+++ src/app/enviroment/firebase-config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAPbAH3DYUMSdjjLADJKoz5Et3qTybHPYU",
+  authDomain: "loan-optimizer-14f73.firebaseapp.com",
+  projectId: "loan-optimizer-14f73",
+  storageBucket: "loan-optimizer-14f73.firebasestorage.app",
+  messagingSenderId: "378596065253",
+  appId: "1:378596065253:web:ae167bf92d581a2ca2390c",
+  measurementId: "G-ZDVTFTL35S"
+};
