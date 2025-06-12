5cbc1dcd1e7fe18563ccd55f02c708103c6ad640
Bohdan Petrenko <bohdan.petrenko.web@gmail.com>
2025-06-12 18:54:16 +0200

    feat(auth): implement RegisterComponent using reactive forms and Firebase Auth for user registration

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatError, MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    CommonModule,
+    MatError,
+  ],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.scss',
+})
+export class RegisterComponent {
+  registerForm;
+
+  constructor(private fb: FormBuilder, private authService: AuthService) {
+    this.registerForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onSubmit() {
+    const email = this.registerForm.value.email?.trim();
+    const password = this.registerForm.value.password;
+
+    if (this.registerForm.valid) {
+      this.authService.signUp(email!, password!).subscribe();
+      alert('Registration successful');
+    }
+  }
+}
