39ccf6ca554a957f7e95627713f50849b66c087c
Bohdan Petrenko <bohdan.petrenko.web@gmail.com>
2025-06-15 14:32:05 +0200

    feat(auth): add registration form with validation in register dialog
    - Created form with email and password fields
    - Added Angular form validation and error messages
    - Submit button disabled until form is valid

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -6,6 +6,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatError, MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { CommonModule } from '@angular/common';
+import { MatDialogModule } from '@angular/material/dialog';
+
 
 @Component({
   selector: 'app-register',
@@ -18,6 +20,7 @@ import { CommonModule } from '@angular/common';
     MatInputModule,
     CommonModule,
     MatError,
+		MatDialogModule,
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
